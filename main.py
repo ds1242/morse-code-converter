1fee28bdf0b4bce41f91c985c3f8babaf4ed43f6
David Shaw <dshaw11@utk.edu>
2024-02-24 09:16:35 -0500

    take user input, split string and store into a list, created function to split word into characters

---
+++ main.py
@@ -0,0 +1,11 @@
+
+
+def split_words_letters(array):
+    output = []
+    for i in array:
+        print(list(i))
+
+
+user_input  = input("Please enter a value to be converted to Morse Code: \n").split()
+
+split_words_letters(user_input)
